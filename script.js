a5bcd371d5479539e495e22963dab146261c7fa1
bartoszmusial91 <bartoszmusial91@gmail.com>
2026-06-21 10:36:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const year = document.querySelector("#year");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+
+year.textContent = new Date().getFullYear();
